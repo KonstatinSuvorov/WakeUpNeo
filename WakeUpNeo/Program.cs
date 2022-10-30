cdd6fbe0a3f48ab2e5f134c649b71de70d88e6f9
Константин Суворов <kniaze84@outlook.com>
2022-10-30 23:14:25 +0300

    Добавьте файлы проекта.

---
+++ WakeUpNeo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WakeUpNeo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Проснись, Нео!!!");
+            Console.ReadKey();
+            Console.WriteLine("Следуй за кроликом");
+            Console.WriteLine("(\\__/)\r\n(='.'=)\r\n(\")_(\")");
+            Console.ReadKey();
+            Console.WriteLine("Тук,тук...");
+            Console.ReadKey();
+        }
+    }
+}
